0b47b7f14155836d662cb92362ea404addef128d
Hanniel1102 <hanbang.tuyet0@gmail.com>
2024-10-14 14:15:34 +0700

    aaa

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Project_QuanLyThuVienPhim
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello!!")
+            Console.WriteLine("Hello!!");
         }
     }
 }
